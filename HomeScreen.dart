aacc4f7543646ee696253450ff79bfb31630d034
ShrutiGhoniya <121309035+ShrutiGhoniya@users.noreply.github.com>
2024-02-14 03:13:15 +0530

    HomeScreen update
    errors

--- HomeScreen.dart
+++ HomeScreen.dart
@@ -1,32 +1,112 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pytorch/flutter_pytorch.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late ModelObjectDetection _objectModel;
+  List<ResultObjectDetection?> objDetect = [];
+  File? _image;
+  ImagePicker _picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+  Future<void> loadModel() async {
+    String pathObjectDetectionModel = "assets/models/yolov5s.torchscript";
+    try {
+      _objectModel = await FlutterPytorch.loadObjectDetectionModel(
+        pathObjectDetectionModel,
+        80, // Number of classes (adjust based on your model)
+        640, // Input image width
+        640, // Input image height
+        labelPath: "assets/labels/labels.txt",
+      );
+    } catch (e) {
+      if (e is PlatformException) {
+        print("Only supported for Android. Error: $e");
+      } else {
+        print("Error: $e");
+      }
+    }
+  }
+
+  Future<void> runObjectDetection() async {
+    final XFile? image = await _picker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 200,
+      maxHeight: 200,
+    );
+
+    objDetect = await _objectModel.getImagePrediction(
+      await File(image!.path).readAsBytes(),
+      minimumScore: 0.1,
+      IOUThreshold: 0.3,
+    );
+
+    objDetect.forEach((element) {
+      print({
+        "score": element?.score,
+        "className": element?.className,
+        "class": element?.classIndex,
+        "rect": {
+          "left": element?.rect.left,
+          "top": element?.rect.top,
+          "width": element?.rect.width,
+          "height": element?.rect.height,
+          "right": element?.rect.right,
+          "bottom": element?.rect.bottom,
+        },
+      });
+    });
+
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("OBJECT DETECTOR APP")),
       backgroundColor: Colors.white,
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //Image with Detections....
-          //Button to click pic
-          ElevatedButton(
-            onPressed: () {},
-            child: const Icon(Icons.camera),
-          )
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Image with Detections or Placeholder
+            Expanded(
+              child: Container(
+                height: 150,
+                width: 300,
+                child: _image != null
+                    ? objDetect.isNotEmpty
+                        ? _objectModel.renderBoxesOnImage(_image!, objDetect)
+                        : Image.file(_image!)
+                    : const Text('No image selected.'),
+              ),
+            ),
+            // Button to click pic
+            ElevatedButton(
+              onPressed: () {
+                runObjectDetection();
+              },
+              child: const Icon(Icons.camera),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
